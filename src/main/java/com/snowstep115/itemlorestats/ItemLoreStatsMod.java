668c9afc633cab604aa09671f1fc0903edb62910
kei-g <km.8k6ce+github@gmail.com>
2019-09-08 21:21:37 +0900

    :tada: Initial commit

---
+++ src/main/java/com/snowstep115/itemlorestats/ItemLoreStatsMod.java
@@ -0,0 +1,8 @@
+package com.snowstep115.itemlorestats;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ItemLoreStatsMod.MODID)
+public final class ItemLoreStatsMod {
+    public static final String MODID = "itemlorestats";
+}
