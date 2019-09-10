1583192847cb7e8c16dd16f9b72da1e5a7db8a1a
kei-g <km.8k6ce+github@gmail.com>
2019-09-10 21:55:05 +0900

    :bookmark: Version 1.0.0

--- src/main/java/com/snowstep115/itemlorestats/IlsMod.java
+++ src/main/java/com/snowstep115/itemlorestats/IlsMod.java
@@ -42,7 +42,7 @@ public final class IlsMod {
     public static final Logger LOGGER = LogManager.getLogger(IlsMod.MODID);
     public static final String MODID = "itemlorestats";
     public static final String NAME = "Item Lore Stats";
-    public static final String VERSION = "HEAD";
+    public static final String VERSION = "1.0.0";
 
     public static <T> T execute(ThrowableSupplier<T> task, Supplier<T> alternate) {
         try {
