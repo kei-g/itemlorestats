783d966a2218de9e63af54e6ee1d96c377b2efb2
kei-g <km.8k6ce+github@gmail.com>
2019-09-10 21:50:21 +0900

    :recycle: Refactoring

--- src/main/java/com/snowstep115/itemlorestats/command/CreateLoreCommand.java
+++ src/main/java/com/snowstep115/itemlorestats/command/CreateLoreCommand.java
@@ -10,6 +10,7 @@ import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.MinecraftServer;
 
@@ -24,36 +25,25 @@ public final class CreateLoreCommand extends CommandBase {
         return "ils createlore";
     }
 
+    private static void addLoreItem(EntityPlayerMP entity, Item item, Lore... lores) {
+        ItemStack itemstack = new ItemStack(item);
+        for (Lore lore : lores)
+            lore.applyTo(itemstack);
+        Lore.deserialize(itemstack, Lore.addDisplayLore(itemstack));
+        entity.addItemStackToInventory(itemstack);
+    }
+
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
         EntityPlayerMP mp = (EntityPlayerMP) sender;
         String subcommand = args[0];
         switch (subcommand) {
         case "createlore":
-            ItemStack sword = new ItemStack(Items.IRON_SWORD);
-            new DamageLore().applyTo(sword);
-            Lore.deserialize(sword, Lore.addDisplayLore(sword));
-            mp.addItemStackToInventory(sword);
-            ItemStack helmet = new ItemStack(Items.LEATHER_HELMET);
-            new ArmourLore().applyTo(helmet);
-            new DodgeLore().applyTo(helmet);
-            Lore.deserialize(helmet, Lore.addDisplayLore(helmet));
-            mp.addItemStackToInventory(helmet);
-            ItemStack chestplate = new ItemStack(Items.LEATHER_CHESTPLATE);
-            new ArmourLore().applyTo(chestplate);
-            new DodgeLore().applyTo(chestplate);
-            Lore.deserialize(chestplate, Lore.addDisplayLore(chestplate));
-            mp.addItemStackToInventory(chestplate);
-            ItemStack leggings = new ItemStack(Items.LEATHER_LEGGINGS);
-            new ArmourLore().applyTo(leggings);
-            new DodgeLore().applyTo(leggings);
-            Lore.deserialize(leggings, Lore.addDisplayLore(leggings));
-            mp.addItemStackToInventory(leggings);
-            ItemStack boots = new ItemStack(Items.LEATHER_BOOTS);
-            new ArmourLore().applyTo(boots);
-            new DodgeLore().applyTo(boots);
-            Lore.deserialize(boots, Lore.addDisplayLore(boots));
-            mp.addItemStackToInventory(boots);
+            addLoreItem(mp, Items.IRON_SWORD, new DamageLore());
+            addLoreItem(mp, Items.LEATHER_HELMET, new ArmourLore(), new DodgeLore());
+            addLoreItem(mp, Items.LEATHER_CHESTPLATE, new ArmourLore(), new DodgeLore());
+            addLoreItem(mp, Items.LEATHER_LEGGINGS, new ArmourLore(), new DodgeLore());
+            addLoreItem(mp, Items.LEATHER_BOOTS, new ArmourLore(), new DodgeLore());
             break;
         }
     }
