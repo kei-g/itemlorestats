d3744f1834c4df75a56b6a60e255071832f61f80
kei-g <km.8k6ce+github@gmail.com>
2019-09-17 06:01:13 +0900

    :bookmark: Version 1.0.13

--- src/main/java/com/snowstep115/itemlorestats/IlsMod.java
+++ src/main/java/com/snowstep115/itemlorestats/IlsMod.java
@@ -52,7 +52,7 @@ public final class IlsMod {
     public static final Logger LOGGER = LogManager.getLogger(IlsMod.MODID);
     public static final String MODID = "itemlorestats";
     public static final String NAME = "Item Lore Stats";
-    public static final String VERSION = "HEAD";
+    public static final String VERSION = "1.0.13";
 
     public static void combatlog(Entity entity, String format, Object... args) {
         if (IlsConfig.displayPositionOfCombatLog == CombatLog.none)
